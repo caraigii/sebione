94efe82aa1e1ad7876d9f80724f6fb3c860d4061
0019caraigii <77878950+0019caraigii@users.noreply.github.com>
2023-02-06 23:23:59 +0800

    initial commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,24 +17,46 @@
     <div class="content">
         <div class="container-fluid">
             <div class="row">
-                <div class="col-lg-12">
+                <div class="col-lg-6">
                     <div class="card">
                         <div class="card-body">
                             <p class="card-text">
-                                <div class="row">
-                                    <div class="col-md-3 col-sm-6 col-12">
-                                                <div class="info-box shadow-none">
-                                                  <span class="info-box-icon bg-info"><i class="far fa-envelope"></i></span>
-                                    
-                                                  <div class="info-box-content">
-                                                    <span class="info-box-text">Shadows</span>
-                                                    <span class="info-box-number">None</span>
-                                                  </div>
-                                                  <!-- /.info-box-content -->
-                                                </div>
-                                                <!-- /.info-box -->
-                                              </div>
+                                <div class="d-flex flex-row">
+                                    <div class="p-2">
+                                        <div class="info-box shadow-none">
+                                            <span class="info-box-icon bg-info"><i class="far fa-envelope"></i></span>
+                              
+                                            <div class="info-box-content">
+                                              <span class="info-box-text">Shadows</span>
+                                              <span class="info-box-number">None</span>
+                                            </div>
+                                            <!-- /.info-box-content -->
+                                          </div>
                                     </div>
+                                  </div>
+                                
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="card">
+                        <div class="card-body">
+                            <p class="card-text">
+                                <div class="d-flex flex-row">
+                                    <div class="p-2">
+                                        <div class="info-box shadow-none">
+                                            <span class="info-box-icon bg-info"><i class="far fa-envelope"></i></span>
+                              
+                                            <div class="info-box-content">
+                                              <span class="info-box-text">Shadows</span>
+                                              <span class="info-box-number">None</span>
+                                            </div>
+                                            <!-- /.info-box-content -->
+                                          </div>
+                                    </div>
+                                  </div>
+                                
                             </p>
                         </div>
                     </div>
